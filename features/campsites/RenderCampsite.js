f6ccbd7ca5ded06027357403cf7d104e8462a943
Kyle Joe <joeconsult2003@gmail.com>
2026-09-09 23:29:54 -0400

    Icons, Favorites, and Comments

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,31 +1,66 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Card } from 'react-native-elements';
+import { FontAwesome } from '@expo/vector-icons';
 
-const RenderCampsite = ({ campsite }) => {
+const RenderCampsite = (props) => {
+    const { campsite } = props;
+
     if (campsite) {
         return (
-            <Card containerStyle={{ padding: 0 }}>
+            <Card containerStyle={styles.cardContainer}>
                 <Card.Image source={campsite.image}>
-                    <View style={{ justifyContent: 'center', flex: 1 }}>
-                        <Text
-                            style={{
-                                color: 'white',
-                                textAlign: 'center',
-                                fontSize: 20
-                            }}
-                        >
-                            {campsite.name}
-                        </Text>
-                    </View>
+                    <Text
+                        style={{
+                            color: '#fff',
+                            textAlign: 'center',
+                            fontSize: 20
+                        }}
+                    >
+                        {campsite.name}
+                    </Text>
                 </Card.Image>
+
                 <Text style={{ margin: 20 }}>
                     {campsite.description}
                 </Text>
+
+                <TouchableOpacity
+                    style={styles.favoriteButton}
+                    onPress={() =>
+                        props.isFavorite
+                            ? console.log('Already set as a favorite')
+                            : props.markFavorite()
+                    }
+                >
+                    <FontAwesome
+                        name={props.isFavorite ? 'heart' : 'heart-o'}
+                        size={24}
+                        color='#fff'
+                    />
+                </TouchableOpacity>
             </Card>
         );
     }
 
-    return <View />;
+    return <></>;
 };
+
+const styles = StyleSheet.create({
+    cardContainer: {
+        padding: 0,
+        margin: 0,
+        marginBottom: 20
+    },
+    favoriteButton: {
+        backgroundColor: '#f50',
+        width: 52,
+        height: 52,
+        borderRadius: 26,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 8,
+        elevation: 4
+    }
+});
 
 export default RenderCampsite;

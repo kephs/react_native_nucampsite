2b130ce93919d4db42237e3d8c6a5687dc1fa635
Kyle Joe <joeconsult2003@gmail.com>
2026-09-10 22:55:15 -0400

    Custom Side Drawer

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,22 +1,54 @@
-import { ScrollView, Text } from 'react-native';
-import { Card } from 'react-native-elements';
+import {
+    ScrollView,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
 
 const ContactScreen = () => {
     return (
         <ScrollView>
-            <Card wrapperStyle={{ margin: 20 }}>
-                <Card.Title>Contact Information</Card.Title>
-                <Card.Divider />
+            <View style={styles.card}>
+                <Text style={styles.cardTitle}>
+                    Contact Information
+                </Text>
+
+                <View style={styles.divider} />
 
                 <Text>1 Nucamp Way</Text>
                 <Text>Seattle, WA 98001</Text>
-                <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+                <Text style={{ marginBottom: 10 }}>
+                    U.S.A.
+                </Text>
 
                 <Text>Phone: 1-206-555-1234</Text>
                 <Text>Email: campsites@nucamp.co</Text>
-            </Card>
+            </View>
         </ScrollView>
     );
 };
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: '#fff',
+        margin: 20,
+        padding: 15,
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 4
+    },
+    cardTitle: {
+        textAlign: 'center',
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#43484d',
+        marginBottom: 10
+    },
+    divider: {
+        height: 1,
+        backgroundColor: '#ddd',
+        marginBottom: 10
+    }
+});
 
 export default ContactScreen;
--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,7 +1,19 @@
-import { Platform, StyleSheet, View } from 'react-native';
+import {
+    Image,
+    Platform,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
 import Constants from 'expo-constants';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {
+    createDrawerNavigator,
+    DrawerContentScrollView,
+    DrawerItemList
+} from '@react-navigation/drawer';
+
 import { FontAwesome } from '@expo/vector-icons';
 
 import HomeScreen from './HomeScreen';
@@ -9,6 +21,8 @@ import DirectoryScreen from './DirectoryScreen';
 import CampsiteInfoScreen from './CampsiteInfoScreen';
 import AboutScreen from './AboutScreen';
 import ContactScreen from './ContactScreen';
+
+import logo from '../assets/images/logo.png';
 
 const Drawer = createDrawerNavigator();
 
@@ -119,6 +133,30 @@ const ContactNavigator = () => {
     );
 };
 
+const CustomDrawerContent = (props) => (
+    <DrawerContentScrollView {...props}>
+        <View style={styles.drawerHeader}>
+            <View style={{ flex: 1 }}>
+                <Image
+                    source={logo}
+                    style={styles.drawerImage}
+                />
+            </View>
+
+            <View style={{ flex: 2 }}>
+                <Text style={styles.drawerHeaderText}>
+                    Nucamp
+                </Text>
+            </View>
+        </View>
+
+        <DrawerItemList
+            {...props}
+            labelStyle={{ fontWeight: 'bold' }}
+        />
+    </DrawerContentScrollView>
+);
+
 const Main = () => {
     return (
         <View
@@ -130,81 +168,86 @@ const Main = () => {
                         : Constants.statusBarHeight
             }}
         >
-<Drawer.Navigator
-    initialRouteName='HomeNav'
-    screenOptions={{
-        drawerStyle: { backgroundColor: '#CEC8FF' },
-        headerShown: true
-    }}
->
-    <Drawer.Screen
-        name='HomeNav'
-        component={HomeNavigator}
-        options={{
-            title: 'Home',
-            headerShown: false,
-            drawerIcon: ({ color }) => (
-                <FontAwesome
-                    name='home'
-                    size={24}
-                    style={{ width: 24 }}
-                    color={color}
+            <Drawer.Navigator
+                initialRouteName='HomeNav'
+                drawerContent={(props) => (
+                    <CustomDrawerContent {...props} />
+                )}
+                screenOptions={{
+                    drawerStyle: {
+                        backgroundColor: '#CEC8FF'
+                    },
+                    headerShown: true
+                }}
+            >
+                <Drawer.Screen
+                    name='HomeNav'
+                    component={HomeNavigator}
+                    options={{
+                        title: 'Home',
+                        headerShown: false,
+                        drawerIcon: ({ color }) => (
+                            <FontAwesome
+                                name='home'
+                                size={24}
+                                style={{ width: 24 }}
+                                color={color}
+                            />
+                        )
+                    }}
                 />
-            )
-        }}
-    />
 
-    <Drawer.Screen
-        name='DirectoryNav'
-        component={DirectoryNavigator}
-        options={{
-            title: 'Campsite Directory',
-            headerShown: false,
-            drawerIcon: ({ color }) => (
-                <FontAwesome
-                    name='list'
-                    size={24}
-                    style={{ width: 24 }}
-                    color={color}
+                <Drawer.Screen
+                    name='DirectoryNav'
+                    component={DirectoryNavigator}
+                    options={{
+                        title: 'Campsite Directory',
+                        headerShown: false,
+                        drawerIcon: ({ color }) => (
+                            <FontAwesome
+                                name='list'
+                                size={24}
+                                style={{ width: 24 }}
+                                color={color}
+                            />
+                        )
+                    }}
                 />
-            )
-        }}
-    />
 
-    <Drawer.Screen
-        name='AboutNav'
-        component={AboutNavigator}
-        options={{
-            title: 'About Us',
-            headerShown: false,
-            drawerIcon: ({ color }) => (
-                <FontAwesome
-                    name='info-circle'
-                    size={24}
-                    style={{ width: 24 }}
-                    color={color}
+                <Drawer.Screen
+                    name='AboutNav'
+                    component={AboutNavigator}
+                    options={{
+                        title: 'About Us',
+                        headerShown: false,
+                        drawerIcon: ({ color }) => (
+                            <FontAwesome
+                                name='info-circle'
+                                size={24}
+                                style={{ width: 24 }}
+                                color={color}
+                            />
+                        )
+                    }}
                 />
-            )
-        }}
-    />
 
-    <Drawer.Screen
-        name='ContactNav'
-        component={ContactNavigator}
-        options={{
-            title: 'Contact Us',
-            headerShown: false,
-            drawerIcon: ({ color }) => (
-                <FontAwesome
-                    name='address-card'
-                    size={24}
-                    style={{ width: 24 }}
-                    color={color}
+                <Drawer.Screen
+                    name='ContactNav'
+                    component={ContactNavigator}
+                    options={{
+                        title: 'Contact Us',
+                        headerShown: false,
+                        drawerIcon: ({ color }) => (
+                            <FontAwesome
+                                name='address-card'
+                                size={24}
+                                style={{ width: 24 }}
+                                color={color}
+                            />
+                        )
+                    }}
                 />
-            )
-        }}
-    />
-</Drawer.Navigator>
+            </Drawer.Navigator>
         </View>
     );
 };
@@ -214,6 +257,24 @@ const styles = StyleSheet.create({
         marginLeft: 10,
         color: '#fff',
         fontSize: 24
+    },
+    drawerHeader: {
+        backgroundColor: '#5637DD',
+        height: 140,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row'
+    },
+    drawerHeaderText: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    drawerImage: {
+        margin: 10,
+        height: 60,
+        width: 60
     }
 });
 

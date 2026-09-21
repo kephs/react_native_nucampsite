f15ae796a9481487f4155d07ddc28d4e195de77f
Kyle Joe <joeconsult2003@gmail.com>
2026-09-20 20:04:55 -0500

    Gesture Part 1

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,9 +1,51 @@
-import { View, StyleSheet, Text } from 'react-native';
+import {
+    View,
+    StyleSheet,
+    Text,
+    PanResponder,
+    Alert
+} from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
 
 const RenderCampsite = (props) => {
     const { campsite } = props;
+
+    const isLeftSwipe = ({ dx }) => dx < -200;
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderEnd: (e, gestureState) => {
+            console.log(gestureState);
+
+            if (isLeftSwipe(gestureState)) {
+                Alert.alert(
+                    'Add Favorite',
+                    'Are you sure you wish to add ' +
+                        campsite.name +
+                        ' to favorites?',
+                    [
+                        {
+                            text: 'Cancel',
+                            style: 'cancel',
+                            onPress: () =>
+                                console.log('Cancel Pressed')
+                        },
+                        {
+                            text: 'OK',
+                            onPress: () =>
+                                props.isFavorite
+                                    ? console.log(
+                                          'Already set as a favorite'
+                                      )
+                                    : props.markFavorite()
+                        }
+                    ],
+                    { cancelable: false }
+                );
+            }
+        }
+    });
 
     if (campsite) {
         return (
@@ -11,6 +53,7 @@ const RenderCampsite = (props) => {
                 animation='fadeInDownBig'
                 duration={2000}
                 delay={1000}
+                {...panResponder.panHandlers}
             >
                 <Card containerStyle={{ padding: 0 }}>
                     <Card.Image source={campsite.image}>
